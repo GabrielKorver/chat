42fbe9051292833266ae4514fec45e5d87f78631
Gabriel de Almeida <gabrielkorver76ers@gmail.com>
2025-12-04 02:11:29 -0300

    ajuste render

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: "*" } });
 const { Pool } = require('pg');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+
 
 // Configuração do Neon/Postgres
 const pool = new Pool({
